a0d908c8dc1e9ae746041d1beea36e2db34dfb3f
CodeOlga <111949555+CodeOlga@users.noreply.github.com>
2022-10-15 11:12:57 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
+import { useState } from 'react';
+import { data } from './data'
 import './App.css';
 
 function App() {
+  const [slide, setSlide] = useState(0);
+  const [showMore, setShowMore] = useState(false)
+  const { id, city, description, image } = data[slide];
+  const previousSlide = () => {
+    setSlide ((slide => {
+      slide --;
+      if (slide < 0){
+        return data.length - 1;
+      }
+      return slide;
+    }))
+  }
+  const nextSlide = () => {
+    setSlide((slide => {
+      slide ++;
+      if (slide > data.length -1) {
+        slide = 0;
+      }
+      return slide;
+    }))
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+    <div>
+      <div className="container">
+        <h1>My 5 favorite cities in the world</h1>
+      </div>
+      <div className="container">
+        <h2>{id} - {city}</h2>
+      </div>
+      <div className="container">
+        <img src = { image } width="300px" alt={city}/>
+      </div>
+      <div className="container">
+        <p>{ showMore ? description : description.substring(0, 200) + '....'}
+        <button className="btn_show_more" onClick={() => setShowMore (!showMore)}>{ showMore ? "show less" : "show more"}</button>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      </div>
+      <div className="btn container">
+        <button onClick={previousSlide} className="button">Previous</button>
+        <button onClick={nextSlide} className="button">Next</button>
+      </div>
     </div>
   );
 }
---
+++ src/data.js
@@ -0,0 +1,39 @@
+export const data = [
+    {
+        id: 1,
+        city: "Kyiv",
+        description: `Kyiv is the capital and most populous city of Ukraine. It is in north-central Ukraine along the Dnieper River. As of 1 January 2021, its population was 2,962,180, making Kyiv the seventh-most populous city in Europe.
+        Kyiv is an important industrial, scientific, educational, and cultural center in Eastern Europe. It is home to many high-tech industries, higher education institutions, and historical landmarks.`,
+        image: "https://unsplash.com/photos/d5sf4SwzOeo/download?ixid=MnwxMjA3fDB8MXxzZWFyY2h8MzJ8fGt5aXZ8ZW58MHx8fHwxNjY1NDEwMDEz&force=true"
+    },
+    {
+        id: 2,
+        city: "Riga",
+        description:`Riga is the capital and largest city of Latvia and is home to 605,802 inhabitants which is a third of Latvia's population. It is also the second largest city in the three Baltic states after Vilnius and is home to one tenth of the three Baltic states' combined population.
+        The city lies on the Gulf of Riga at the mouth of the Daugava river where it meets the Baltic Sea. Riga was founded in 1201 and is a former Hanseatic League member. Riga's historical centre is a UNESCO World Heritage Site, noted for its Art Nouveau/Jugendstil architecture and 19th century wooden architecture.`,
+        image: "https://unsplash.com/photos/Et3jz7Od29g/download?ixid=MnwxMjA3fDB8MXxzZWFyY2h8MTl8fHJpZ2F8ZW58MHx8fHwxNjY1NDc3MDYz&force=true"
+    },
+    {
+        id: 3,
+        city: "San Diego",
+        description:`San Diego is a city on the Pacific Ocean coast of Southern California located immediately adjacent to the Mexico–United States border. With a 2020 population of 1,386,932, it is also the eighth most populous city in the United States and the seat of San Diego County,
+        the fifth most populous county in the United States, with 3,338,330 estimated residents as of 2019. The city is known for its mild year-round climate, natural deep-water harbor, extensive beaches and parks, long association with the U.S. armed forces, and recent emergence as a healthcare and biotechnology development center. 
+        San Diego is the second largest city in the state of California after Los Angeles.`,
+        image: "https://unsplash.com/photos/GfCf1Qs5YVs/download?ixid=MnwxMjA3fDB8MXxzZWFyY2h8NjF8fHNhbiUyMGRpZWdvfGVufDB8fHx8MTY2NTQxMzM5OQ&force=true"
+    },
+    {
+        id: 4,
+        city: "Los Angeles",
+        description:`Los Angeles, often referred to by its initials L.A., is the commercial, financial, and cultural center of Southern California. With a population of roughly 3.9 million as of 2020, it is also the largest city in the state of California and the second most populous city in the United States after New York City,
+        as well as one of the world's most populous megacities. Los Angeles is known for its Mediterranean climate, ethnic and cultural diversity, Hollywood film industry, and sprawling metropolitan area. The city of Los Angeles lies in a basin in Southern California adjacent to the Pacific Ocean extending through the Santa Monica Mountains and into the San Fernando Valley. `,
+        image: "https://unsplash.com/photos/-hDsIhpgCdU/download?ixid=MnwxMjA3fDB8MXxzZWFyY2h8M3x8bG9zJTIwYW5nZWxlc3xlbnwwfHx8fDE2NjU0ODI0Nzc&force=true"
+    },
+    {
+        id: 5,
+        city: "New York City",
+        description:`New York, often called New York City (NYC) to distinguish it from the State of New York, is the most populous city in the United States. With a 2020 population of 8,804,190 distributed over 300.46 square miles (778.2 km2), New York City is also the most densely populated major city in the United States.
+        New York City is a global cultural, financial, and media center with a significant influence on commerce, entertainment, research, technology, education, politics, tourism, dining, art, fashion, and sports. New York is the most photographed city in the world. Home to the headquarters of the United Nations, New York is an important center for international diplomacy,
+        an established safe haven for global investors, and is sometimes described as the capital of the world.`,
+        image: "https://unsplash.com/photos/IA8FR0RyJDE/download?force=true"
+    }
+]
